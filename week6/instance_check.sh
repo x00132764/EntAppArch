e3e6763c00ec66d0e9283ca8631d4771fe07c95b
epa <epa@localhost.localdomain>
2017-10-27 11:04:41 +0100

    changed shell script, and ran cron command to run the changed script (command inclued as comment

--- week6/instance_check.sh
+++ week6/instance_check.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-cnt=`aws ec2 describe-instance-status --instance-ids $1 | grep running | wc -l`
+#* * * * * * /home/epa/EPA/EntAppArch/week6/instance_check.sh
+cnt1=`aws ec2 describe-instance-status --instance-ids i-033390d4b2e4fb049 | grep running | wc -l`
 
-if [ $cnt -gt 0 ]; then
+if [ $cnt1 -gt 0 ]; then
     echo "instance running"
 else
-    echo "instance not running"
+    echo "i-033390d4b2e4fb049 not running" >> instanceHealthCheck.txt
 fi
+
+cnt2=`aws ec2 describe-instance-status --instance-ids i-07bc96b8192810fe0 | grep running | wc -l`
+
+if [ $cnt2 -gt 0 ]; then
+    echo "instance running"
+else
+    echo "i-07bc96b8192810fe0 not running" >> instanceHealthCheck.txt
+fi
+
